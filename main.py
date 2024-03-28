486f0b96969777d487cf82fb69dce621007344e5
Mark Johnston <markjaz@users.noreply.github.com>
2024-03-28 16:54:21 -0600

    Added invoice date and borders.

--- main.py
+++ main.py
@@ -16,8 +16,17 @@ for filepath in filepaths:
     pdf.add_page()
     # Extract the filename without extension.
     filename = Path(filepath).stem
-    invoice_number = filename.split("-")[0]
+    invoice_number, invoice_date = filename.split("-")
+
     pdf.set_font("Helvetica", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice no: {invoice_number}")
+    # The 'ln=1' parameter moves the cursor to the next line below the cell.
+    pdf.cell(w=50, h=8, txt=f"Invoice number: ", align='R', border=True,
+             ln=0)
+    pdf.cell(w=50, h=8, txt=invoice_number, align='L', border=True, ln=1)
+
+    pdf.set_font("Helvetica", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice date: ", align='R', border=True, ln=0)
+    pdf.cell(w=50, h=8, txt=invoice_date, align='L', border=True, ln=1)
+
     pdf.output(f"PDF Invoices/{filename}.pdf")
     print(f"Created PDF Invoices/{filename}.pdf")
