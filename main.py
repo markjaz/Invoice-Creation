f906ce488d3ec0a0b8771216d423026d0d7a9380
Mark Johnston <markjaz@users.noreply.github.com>
2024-03-28 18:21:15 -0600

    Added table of purchased items and their prices.

--- main.py
+++ main.py
@@ -9,24 +9,56 @@ import pandas as pd
 
 # Create a Python list containing the file names of items in '/invoices'.
 filepaths = glob.glob('Excel files/*.xlsx')
-print(filepaths)
+# print(filepaths)
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name='Sheet 1')
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
     # Extract the filename without extension.
     filename = Path(filepath).stem
+
+    # Get invoice number and date from the filename.
     invoice_number, invoice_date = filename.split("-")
-
+    # Create a sort of header containing invoice number and date.
     pdf.set_font("Helvetica", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice number: ", align='R', border=False)
+    pdf.cell(w=50, h=8, txt=invoice_number, align='L', border=False, ln=1)
     # The 'ln=1' parameter moves the cursor to the next line below the cell.
-    pdf.cell(w=50, h=8, txt=f"Invoice number: ", align='R', border=True,
-             ln=0)
-    pdf.cell(w=50, h=8, txt=invoice_number, align='L', border=True, ln=1)
-
     pdf.set_font("Helvetica", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice date: ", align='R', border=True, ln=0)
-    pdf.cell(w=50, h=8, txt=invoice_date, align='L', border=True, ln=1)
+    pdf.cell(w=50, h=8, txt=f"Invoice date: ", align='R', border=False)
+    pdf.cell(w=50, h=8, txt=invoice_date, align='L', border=False)
+
+    # Note that if we were OK with the preset column names and just needed
+    # to replace the underscores with spaces and capitalize the names,
+    # a list comprehension would do it.  Since 'df.columns' is an iterable
+    # object, we can replace underscores and capitalize with this list
+    # comprehension:
+    # col_names = [item.replace("_", " ").title() for item in df.columns]
+
+    df = pd.read_excel(filepath, sheet_name='Sheet 1')
+    # Create the column header row
+    pdf.set_xy(10, 40)
+    pdf.set_font("Helvetica", size=12, style='B')
+    pdf.set_text_color(64, 64, 64)
+    pdf.cell(w=25, h=8, txt='Product ID', align='L', border=True)
+    pdf.cell(w=85, h=8, txt='Product Name/Description', align='L', border=True)
+    pdf.cell(w=20, h=8, txt='Quantity', align='R', border=True)
+    pdf.cell(w=30, h=8, txt='Unit Price', align='R', border=True)
+    pdf.cell(w=30, h=8, txt='Total Price', align='R', ln=1, border=True)
+    # Fill in the actual data for each column
+    for index, row in df.iterrows():
+        pdf.set_font("Helvetica", size=10)
+        pdf.set_text_color(96, 96, 96)
+        pdf.cell(w=25, h=8, txt=str(row['product_id']), align='L',
+                 border=True)
+        pdf.cell(w=85, h=8, txt=str(row['product_name']), align='L',
+                 border=True)
+        pdf.cell(w=20, h=8, txt=str(row['amount_purchased']), align='R',
+                 border=True)
+        pdf.cell(w=30, h=8, txt=str(f'$ {row['price_per_unit']:,.2f}'),
+                 align='R',
+                 border=True)
+        pdf.cell(w=30, h=8, txt=str(f'$ {row['total_price']:,.2f}'), align='R',
+                 ln=1, border=True)
 
     pdf.output(f"PDF Invoices/{filename}.pdf")
     print(f"Created PDF Invoices/{filename}.pdf")
